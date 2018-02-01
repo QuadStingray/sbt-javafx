6ef5168b151b089ce5929d67bd0f9e1f4188be87
QuadStingray <github@quadstingray.com>
2018-02-01 08:55:21 +0100

    missed param for git

--- .travis.release.sh
+++ .travis.release.sh
@@ -4,5 +4,11 @@ if grep -q "SNAPSHOT" version.sbt
 then
     echo ""
 else
-    rm -rf credentials.properties && git remote rm origin && git remote add origin git@github.com:QuadStingray/sbt-javafx.git && git checkout master && sbt "release with-defaults" && sbt paradox ghpagesPushSite
+    rm -rf credentials.properties &&
+    git remote rm origin &&
+    git remote add origin git@github.com:QuadStingray/sbt-javafx.git &&
+    git fetch origin master &&
+    git checkout master &&
+    sbt "release with-defaults" &&
+    sbt paradox ghpagesPushSite
 fi
