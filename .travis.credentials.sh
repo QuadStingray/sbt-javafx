4851da54b22efdab4d1e3f145ac0cba79602fd27
QuadStingray <github@quadstingray.com>
2018-02-01 08:26:42 +0100

    removed .travis.credentials.sh

--- .travis.credentials.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir ~/.bintray/
-BINTRAY_FILE=$TRAVIS_BUILD_DIR/credentials.properties
-cat <<EOF >$BINTRAY_FILE
-realm = Bintray API Realm
-host = api.bintray.com
-user = $BINTRAY_USER
-password = $BINTRAY_PASSWORD
-EOF
